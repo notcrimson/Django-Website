7809c1e9389a2717f8d0fbb095e8bb1c0041d087
Artem Sedov <artden1mc@gmail.com>
2022-11-17 16:10:59 +0300

    changing db to postgres

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "polls",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "SitesDB",
+        "USER": "postgres",
+        "PASSWORD": "b7v9m34",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Russia/Moscow"
 
 USE_I18N = True
 
